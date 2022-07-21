31db10a5f19cc120694b990102cee9ca87fa61b9
Oliver KEB <oliver.keb@icloud.com>
2022-07-21 17:20:26 +0200

    :sparkles: feature(terre11): exercice terre numéro 11 terminé

--- terre11.rb
+++ terre11.rb
@@ -1,8 +1,8 @@
 # Créez un programme qui transforme une heure affichée en format 24h en une heure affichée en format 12h.
 data = ARGV[0]
-if ARGV.length == 1 && ARGV[0].match(/^((0?[0-9]{1})|1{1}[0-9]{1}|2{1}[0-4]{1}):{1}[0-5]{1}[0-9]{1}$/)
-  heures = ARGV[0].split(':')[0].to_i
-  minutes = ARGV[0].split(':')[1].to_i
+if ARGV.length == 1 && data.match(/^((0?[0-9]{1})|1{1}[0-9]{1}|2{1}[0-4]{1}):{1}[0-5]{1}[0-9]{1}$/)
+  heures = data.split(':')[0].to_i
+  minutes = data.split(':')[1].to_i
   if heures.zero?
     puts "12:#{minutes}AM"
   elsif heures.positive? && heures < 12
