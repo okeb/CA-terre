def1e7067b245e89c72ba7cbd11a2a4d583a7c92
Oliver KEB <oliver.keb@icloud.com>
2022-07-21 06:51:21 +0200

    :sparkles: feature(terre02): exercice terre numéro 02 terminé

--- terre01.rb
+++ terre01.rb
@@ -1,2 +1,2 @@
 # creer un programme qui affiche son nom de fichier
-puts __FILE__.to_s.split('/').last
+puts $0
---
+++ terre02.rb
@@ -0,0 +1,3 @@
+# Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
+puts $*
+
