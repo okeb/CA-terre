4de03c5c0e2aa2d543d84286a0da152354dedd64
Oliver KEB <oliver.keb@icloud.com>
2022-07-21 17:47:05 +0200

    :sparkles: feature(terre12): exercice terre numéro 12 terminé

--- terre12.rb
+++ terre12.rb
@@ -5,30 +5,16 @@ if ARGV.length == 1 && data.match(/^((0?[1-9]{1})|1{1}[0-2]{1}):{1}([0-5]{1}[0-9
   minutes = data.split(':')[1][0,2]
   meridiem = data.split(':')[1][2,4].downcase
   if meridiem == 'am'
-
     if heures == 12
       puts "00:#{minutes}"
     else
       puts "#{heures}:#{minutes}"
     end
+  elsif heures == 12
+    puts "12:#{minutes}"
   else
-    if heures == 12
-      puts "12:#{minutes}"
-    else
-      puts "#{heures + 12}:#{minutes}"
-    end
+    puts "#{heures + 12}:#{minutes}"
   end
-  # if heures.zero?
-  #   puts "12:#{minutes}AM"
-  # elsif heures.positive? && heures < 12
-  #   puts "#{heures}:#{minutes}AM"
-  # elsif heures == 12
-  #   puts "12:#{minutes}PM"
-  # elsif heures >= 12 && heures < 24
-  #   puts "#{heures - 12}:#{minutes}PM"
-  # else
-  #   puts "12:#{minutes}AM"
-  # end
 else
   puts 'veuillez entrer une heure de la forme hh:mmAM ou hh:mmPM'
 end
