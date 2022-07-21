57723951d32358bbbbdc07443e27743e5bdf6ace
Oliver KEB <oliver.keb@icloud.com>
2022-07-21 06:55:53 +0200

    :sparkles: feature(terre02): exercice terre numéro 02 terminé

--- terre02.rb
+++ terre02.rb
@@ -1,3 +1,3 @@
-# Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
+# Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne
 puts $*
 
