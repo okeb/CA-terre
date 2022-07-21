3447afb56275ab66f9e6a28269cfac9859933e7d
Oliver KEB <oliver.keb@icloud.com>
2022-07-21 11:16:45 +0200

    :sparkles: feature(terre07): exercice terre numéro 07 terminé

---
+++ terre07.rb
@@ -0,0 +1,8 @@
+# Créez un programme qui affiche le nombre de caractères d’une chaîne de caractères passée en argument.
+word = ARGV.length
+
+if ARGV.length == 1 && ARGV[0].to_i.zero?
+  puts ARGV[0].length
+else
+  puts 'erreur'
+end
