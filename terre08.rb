3a5617a566fa6e7cc2ad11edca420ea2b73be0e4
Oliver KEB <oliver.keb@icloud.com>
2022-07-21 13:38:25 +0200

    :sparkles: feature(terre08): exercice terre numéro 08 terminé

--- terre08.rb
+++ terre08.rb
@@ -1,14 +1,14 @@
 # Créez un programme qui affiche le résultat d’une puissance.
 if ARGV.length == 2
-  if ARGV[1].to_i != 0
-    if (ARGV[0].to_i == ARGV[0].to_s.chr.to_i && ARGV[0].to_s.length == 1) || (ARGV[0].to_s.length >= 2 && ARGV[0].chr.to_i >= 1 && ARGV[0].to_i > 10)
+  if (ARGV[1].ord == 48 && ARGV[1].to_i.ord.zero?) || (ARGV[1].ord == 49 && ARGV[1].to_i.ord == 1) || (ARGV[1].to_i.ord.positive? && ARGV[1].to_s.length == ARGV[1].to_i.ord.to_s.length)
+    if (ARGV[0].ord == 48 && ARGV[0].to_i.ord.zero?) || (ARGV[0].ord == 49 && ARGV[0].to_i.ord == 1) || (ARGV[0].to_i.ord != 0 && ARGV[0].to_s.length == ARGV[0].to_i.ord.to_s.length)
       puts ARGV[0].to_i**ARGV[1].to_i
     else
       puts "votre premier argument semble ne pas être un chiffre"
     end
   else
-    puts "l'exposant (le second argument) doit etre différent de 0"
+    puts "l'exposant (le second argument) doit être numérique et supérieur ou égal à zéro"
   end
 else
-  puts "Le programme s'attends a recevoir deux argument"
+  puts "Veuillez passer deux arguments au programme"
 end
--- terre09.rb
+++ terre09.rb
@@ -1,6 +1,23 @@
 # Créez un programme qui affiche la racine carrée d’un entier positif.
-if ARGV.length == 1
-  puts "on peut travailler"
+case ARGV.length
+when 1
+  if ARGV[0].ord == 48 && ARGV[0].to_i.ord.zero?
+    puts '0'
+  elsif ARGV[0].ord == 49 && ARGV[0].to_i.ord == 1
+    puts '1'
+  elsif ARGV[0].to_i.ord.positive? && ARGV[0].to_s.length == ARGV[0].to_i.ord.to_s.length
+    square_root = Math.sqrt(ARGV[0].to_i)
+    if square_root == square_root.to_i
+      puts square_root.to_i
+    else
+      puts square_root
+    end
+  else
+    puts 'Entrez uniquement un chiffre ou un nombre positif comme argument'
+  end
+when 0
+  puts 'Renseignez un argument'
 else
-  puts "Vous devez entrer un seul argument"
+  puts 'Vous devez entrer un seul argument'
 end
+
